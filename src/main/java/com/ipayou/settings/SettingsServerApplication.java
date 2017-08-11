b6e5ef0baef552657d041f073da778fb993fdd30
Vassit <Vassit@VASSIT-0041>
2017-08-11 17:03:42 +0100

    Initial

---
+++ src/main/java/com/ipayou/settings/SettingsServerApplication.java
@@ -0,0 +1,12 @@
+package com.ipayou.settings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SettingsServerApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SettingsServerApplication.class, args);
+	}
+}
